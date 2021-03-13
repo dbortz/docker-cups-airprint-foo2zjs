7852eaf53344efc871a9288d73f4223c23601abb
Markus Hoffmann <markus@webhoffmann.de>
2021-03-13 16:48:15 +0100

    Cleanup

--- scripts/run-cups.sh
+++ scripts/run-cups.sh
@@ -5,15 +5,18 @@ set -x
 if [ $(grep -ci $CUPSADMIN /etc/shadow) -eq 0 ]; then
 	useradd -r -G lpadmin -M $CUPSADMIN
 fi
+
 echo $CUPSADMIN:$CUPSPASSWORD | chpasswd
 
 mkdir -p /config/ppd
 mkdir -p /services
 rm -rf /etc/cups/ppd
 ln -s /config/ppd /etc/cups
+
 if [ ! -f /config/printers.conf ]; then
 	touch /config/printers.conf
 fi
+
 cp /config/printers.conf /etc/cups/printers.conf
 
 /root/printer-update.sh &
